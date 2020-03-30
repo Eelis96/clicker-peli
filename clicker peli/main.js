545be2a2cbb5a9d0cff2cd7d1536843d8246c979
Eelis96 <57720310+Eelis96@users.noreply.github.com>
2020-03-30 10:37:25 +0300

    Add files via upload

--- clicker peli/main.js
+++ clicker peli/main.js
@@ -1,16 +1,20 @@
-var moneycount = 0;
-var autoClick = 0;
-var costAutoClick = 0;
-
 function update()    { 
     document.getElementById('text').value = moneycount;
     document.title = moneycount + " moneys";
     document.getElementById('amountAutoClick').innerHTML = "You Own " + autoClick +" Auto Clickers";
-    document.getElementById('costAutoClick').innerHTML = (autoClick+1) * 12 +" moneys";
+    document.getElementById('costAutoClick').innerHTML = ((autoClick+1)) * 5 +" moneys";
+    document.getElementById('farmCount').innerHTML = "You Own " + farmCount +" farms";
+    document.getElementById('costFarm').innerHTML = ((farmCount+1) * 10) +" moneys";
 }
+
+var moneycount = 0;
+var autoClick = 0;
+var costAutoClick = 0;
+var farmCount = 0;
 
 function timer() {
     moneycount = moneycount + autoClick;
+    moneycount = moneycount + farmCount*2;
     update()
 }
 setInterval(timer, 1000);
@@ -23,20 +27,31 @@ function add() {
 function save() {
     localStorage.setItem("moneycount", moneycount);
     localStorage.setItem("autoClick", autoClick);
+    localStorage.setItem("farmCount", farmCount);
 }
 
   function load() {
-    cookiecount = localStorage.getItem("moneycount");
-    cookiecount = parseInt(moneycount);
+    moneycount = localStorage.getItem("moneycount");
+    moneycount = parseInt(moneycount);
     autoClick = localStorage.getItem("autoClick");
     autoClick = parseInt(autoClick);
+    farmCount = localStorage.getItem("farmCount");
+    farmCount = parseInt(farmCount);
     update()
 }
 
 function buyAutoClick() {
-    if (moneycount >= ((autoClick+1) * 12)) {
-    moneycount = moneycount - ((autoClick+1) * 12);
+    if (moneycount >= ((autoClick+1) * 5)) {
+    moneycount = moneycount - ((autoClick+1) * 5);
     autoClick = autoClick + 1;
+    update()
+    }
+}
+
+function buyFarm() {
+    if (moneycount >= ((farmCount+1) * 10)) {
+    moneycount = moneycount - ((farmCount+1) * 10);
+    farmCount = farmCount + 1;
     update()
     }
 }
